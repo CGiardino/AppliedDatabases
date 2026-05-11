4efff659256214fc5c5c5fefa15f77d6e4c2a41d
Carmine Giardino <carmine.giardino@ocamsoft.com>
2026-05-11 03:56:13 +0100

    choice 1: fix printing session details for speaker

--- services/session_service.py
+++ services/session_service.py
@@ -1,15 +1,17 @@
 from dao.speaker_session_dao import fetch_sessions_by_speaker_name_in_db
+from utils.constants import MENU_SEPARATOR
 
 
 def show_sessions_for_speaker_letters() -> None:
     speaker_name = input('Enter speaker name: ')
     rows = fetch_sessions_by_speaker_name_in_db(speaker_name)
 
+    print(f'Session Details For : {speaker_name}')
+    print(f"{MENU_SEPARATOR}")
     if not rows:
         print('No speakers found of that name.\n')
         return
 
-    print(f'\nSession Details For : {speaker_name}')
     for row in rows:
         print(f"{row['speakerName']} | {row['sessionTitle']} | {row['roomName']}")
     print()
